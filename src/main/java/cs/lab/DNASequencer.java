c321c37be86fa4672208512a90b5b6335499afda
Alejandro  Otero <alejandrotero@Alejandros-MacBook-Air.local>
2020-09-16 12:41:52 -0500

    Lab done

--- src/main/java/cs/lab/DNASequencer.java
+++ src/main/java/cs/lab/DNASequencer.java
@@ -11,7 +11,25 @@ public class  DNASequencer {
     }
 
     public String calculate(List<String> part){
+        
+        String answer = "";
+        String subcadena = "";
+        int k = 0;
 
-        return "AGATTACAGA";
+        answer += part.get(0);
+
+        for(int i = 1; i < part.size(); i++){
+            for(int j = 0; j < answer.length(); j++){
+                if(answer.charAt(j) == part.get(i).charAt(k)){
+                    subcadena += part.get(i).charAt(k);
+                    k++;
+                }
+            }
+            k = 0;
+            answer += part.get(i).substring(subcadena.length(),part.get(i).length());
+            subcadena = "";
+        }
+
+        return answer;
     }
 }
